e4f868320e29b1b927d28727fe854353df88b83a
Михаил <todo@asd.ru>
2023-01-30 01:30:05 +0400

    test

---
+++ app.js
@@ -0,0 +1,23 @@
+// import { app } from "../helpers/runApp.js";
+import express from "express";
+import runMongo from "./src/helpers/mongo.js";
+// import note from "../routes/notes.routes.js";
+// import auth from "../routes/auth.routes.js";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+// app.use("/note", note);
+// app.use("/auth", auth);
+
+void (async () => {
+  console.log("Starting mongo");
+  await runMongo();
+  console.log("Mongo connected");
+  await app.listen(process.env.PORT, () => {});
+})();
+
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
