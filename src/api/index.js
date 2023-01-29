e45077508a8281d61acd69385e65b092c52ae7a2
Михаил <todo@asd.ru>
2023-01-30 00:53:59 +0400

    vercel test 5

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,14 @@
-import { app } from "./helpers/runApp.js";
-import runMongo from "./helpers/mongo.js";
+// import { app } from "../helpers/runApp.js";
+import runMongo from "../helpers/mongo.js";
+import note from "../routes/notes.routes";
+import auth from "../routes/auth.routes.js";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/note", note);
+app.use("/auth", auth);
 
 void (async () => {
   console.log("Starting mongo");
