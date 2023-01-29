dd278ef2475b26e757543717f0f2bfffb53f2b99
Михаил <todo@asd.ru>
2023-01-30 01:49:24 +0400

    test

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
-// // import { app } from "../helpers/runApp.js";
-// import express from "express";
-// import runMongo from "./src/helpers/mongo.js";
-// // import note from "../routes/notes.routes.js";
-// // import auth from "../routes/auth.routes.js";
-// import cors from "cors";
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-// // app.use("/note", note);
-// // app.use("/auth", auth);
-
-// void (async () => {
-//   console.log("Starting mongo");
-//   await runMongo();
-//   console.log("Mongo connected");
-//   await app.listen(process.env.PORT, () => {});
-// })();
-
-// app.get("/", (req, res) => {
-//   res.send("Hey this is my API running 🥳");
-// });
-
-import express from "express";
-
+const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
+app.use(express.json({ extended: false }));
 
-app.listen(1111, () => {
-  console.log("Running on port 5000.");
-});
+app.use("/api/product", product);
 
-// Export the Express API
-export default app;
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
